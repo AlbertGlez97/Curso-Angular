ecb1558a77cf82a9bc8fbbd143a2d400c87367b1
Juan Alberto Gonzalez de la Cruz <juan.gonzalez@syesoftware.com>
2025-06-09 11:23:26 -0600

    feat: Aprendiendo Clases en TypeScript

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,10 +1,13 @@
-import './style.css'
+import "./style.css";
 //import './topics/01-basic-types'
 //import './topics/02-object-interface'
 //import './topics/03-functions'
 //import './topics/04-homework-types'
 //import './topics/05-basic-destruturing'
+//import './topics/06-function-destructuring'
+//import "./topics/07-import-export";
+import "./topics/08-clases";
 
-const app = document.querySelector<HTMLDivElement>('#app')!;
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
-app.innerHTML = `<h1>Hello TypeScript!</h1>`
+app.innerHTML = `<h1>Hello TypeScript!</h1>`;
